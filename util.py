06b959d16e9fd4e4d3003a88756c9c483edfd0f4
Ana Caroline <carol_mb@usp.br>
2021-03-09 16:40:08 -0300

    plots folder + merge (util.py)

--- util.py
+++ util.py
@@ -1,7 +1,6 @@
 import sys
 import getopt
 import numpy as np
-<<<<<<< HEAD
 from read_file import select_original_breakpoints
 
 
@@ -37,49 +36,17 @@ def read_preprocessed_file(N, source, labels_source):
 def get_args_terminal():
     argv = sys.argv[1:]
 
-=======
-
-def read_preprocessed_file(N,source):
-    original = np.loadtxt(source)
-    slopes = original[:,:N]
-    intervals = original[:,N:-1]
-    labels = original[:,-1]
-    labels = labels.astype(int)
-    total_labels = len(set(labels))
-
-    labels_slopes,labels_intervals = [[] for _ in range(total_labels)],[[] for _ in range(total_labels)]
-    for label,s,l in zip(labels,slopes,intervals):
-        
-        labels_slopes[label].append(s)
-        labels_intervals[label].append(l)
-
-    labels_slopes = [np.asarray(values) for values in labels_slopes]
-    labels_intervals = [np.asarray(values) for values in labels_intervals]
-    return labels_slopes,labels_intervals
-
-def get_args_terminal():
-    argv = sys.argv[1:]
-    
->>>>>>> 40acf88380cf52ec60fbc7467bb637a226b280bb
     source = None
     output = None
     N = 5
     preprocessed = False
     try:
-<<<<<<< HEAD
         opts, args = getopt.getopt(argv, "s:o:N:p")
-=======
-        opts,args = getopt.getopt(argv,"s:o:N:p")
->>>>>>> 40acf88380cf52ec60fbc7467bb637a226b280bb
     except getopt.GetoptError:
         print('usage: python example.py -s <source> -o <output> -N <n> -p')
         return None
 
-<<<<<<< HEAD
     for opt, arg in opts:
-=======
-    for opt,arg in opts:
->>>>>>> 40acf88380cf52ec60fbc7467bb637a226b280bb
 
         if opt == '-s':
             source = arg
@@ -90,8 +57,4 @@ def get_args_terminal():
         elif opt == '-p':
             preprocessed = True
 
-<<<<<<< HEAD
     return source, output, N, preprocessed
-=======
-    return source,output,N,preprocessed
->>>>>>> 40acf88380cf52ec60fbc7467bb637a226b280bb
